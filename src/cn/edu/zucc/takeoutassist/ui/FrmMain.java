e45d1b5bb22a8b8d777656aad4a13f24fb83e087
nbaqian <Tracy@MSI>
2020-07-05 14:25:18 +0800

    commit1_

--- src/cn/edu/zucc/takeoutassist/ui/FrmMain.java
+++ src/cn/edu/zucc/takeoutassist/ui/FrmMain.java
@@ -1,5 +1,20 @@
 package cn.edu.zucc.takeoutassist.ui;
 
-public class FrmMain {
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class FrmMain extends JFrame {
 
 }
